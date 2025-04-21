8d6a4f60e090c3eb4c4a3b4018b9ecb8da5669af
Ashley Navos <anavos@calpoly.edu>
2025-04-21 00:40:45 -0700

    table shows up

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form"
   
   function MyApp() {
     const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+      fetchUsers()
+        .then((res) => res.json())
+        .then((json) => setCharacters(json["users_list"]))
+        .catch((error) => {
+          console.log(error);
+        });
+    }, []);
 
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
@@ -29,4 +38,11 @@ import Form from "./Form"
 
 
   }
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+
 export default MyApp;
